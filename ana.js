48a43319f44721d2e07eefcf1006bda9c9cbb64d
jag <jag@pop-os.localdomain>
2023-07-30 21:00:18 -0300

    Ya casi, código mas límpio

--- ana.js
+++ ana.js
@@ -28,6 +28,7 @@ let temp=null
 let anti=null
 let salida=[]
 let i=0
+
 m.forEach((e,index)=>{
 
     if (e!=anti) {
@@ -35,11 +36,17 @@ m.forEach((e,index)=>{
         if (i>0) {
 
             salida.forEach((s)=>{
+                
                 if (s.includes(e)) {
-                    s.push(e)
-                    
+                    s.push(e)                    
                 } 
             })
+            /* if (tmp) {
+                tmp=false
+                salida.push([])
+                i=salida.length-1
+                salida[i].push(e)
+            } */
             /* salida.forEach((s)=>{
                 if(s.includes(e)) {
                     s.push(e)
@@ -61,58 +68,5 @@ m.forEach((e,index)=>{
     }
     console.log(i, e, anti, salida);
     anti=e
-    //i+=
-    //if (index == m.indexOf('aet', index)) ana.push(strs[index]) //funciona!
-    //console.log(e, index)
-    
-    /* (e!=ant) ? console.log('distinto') : console.log('igual')
-
-    console.log(ant, e, index)
-    ant=e */
-    /* if (e != ant) {
-        ana.push(e)
-        ant=e
-    }
-    console.log(e, index, ant) */
     
 })
-//console.log(ana);
-/* let prev=""
-let grouped=[]
-const red=m.reduce((grouped, a, index)=>{
-    if (prev!=a) grouped[index]=[]
-    prev!=a ? grouped=[] : grouped.push(a)
-    grouped[index].push(a)
-    
-    console.log(` ${grouped} ${a}  ${index} `)
-    prev=a
-    
-},[])
- */
-/* m.forEach((e, index)=>{
-    console.log(m.reduce((e,a,i)=>{
-        e===a ?  e : ''
-    }))
-}) */
-/* m.reduce((p,a,i)=>{
-    
-}) */
-/* const t=m.reduce(m.filter((a,index)=>(
-    a===m[0] ? ana.push(strs[index]) : ''
-    ),m))
-
-console.log(t) */
-
-/* m.forEach((value,index)=>{
-    m.filter((a,index2)=>(
-        a===value ? ana.push(strs[index2]) : ''
-    ))
-    anagrama.push(ana)
-    }
-    )
-
-console.log(anagrama) */
-/* console.log(m.filter((a,index)=>(
-  a===m[0] ? ana.push(strs[index]) : ''
-  ), m))
- */
